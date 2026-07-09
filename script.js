991e362122f90fe2189d1cff228d2cdf1e78dd76
profmateuskromp <132761823+profmateuskromp@users.noreply.github.com>
2026-07-09 12:22:36 -0300

    css e js

---
+++ script.js
@@ -0,0 +1,11 @@
+    const botoes = document.querySelectorAll("button");
+
+    botoes.forEach(function(botao){
+        botao.addEventListener("click", botaoClicado);
+         
+        function botaoClicado () {
+            console.log("fui clicado");
+            let texto = botao.querySelector("span");
+            texto.textContent++;
+    }
+    });
