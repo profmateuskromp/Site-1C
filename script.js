ab2364e6fbcf7e7e7f2446435ae7fb4d4428a7a9
profmateuskromp <132761823+profmateuskromp@users.noreply.github.com>
2026-08-27 08:09:31 -0300

    escuro

--- script.js
+++ script.js
@@ -9,3 +9,16 @@
             texto.textContent++;
     }
     });
+
+    const btnTemaEscuro = document.querySelector(".btn-tema-escuro");
+
+    btnTemaEscuro.addEventListener("click", mudoTema);
+
+    function mudoTema() {
+        const corpoPagina = document.body;
+        if (corpoPagina.classList.contains("tema-escuro")) {
+            corpoPagina.classList.remove("tema-escuro");
+        } else {
+            corpoPagina.classList.add("tema-escuro");
+        }
+    }
